9f7a8c48116517de2a780d353d0d9886e06eeb01
Dhananjayarajan <162829820+Dhananjayarajan@users.noreply.github.com>
2025-07-19 23:16:56 +0530

    Home page created

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit {
+
+  private http = inject(HttpClient)
+  protected title = 'Dating App';
+  protected members = signal<any>([])
+
+    async ngOnInit() {
+      this.members.set(await this.getMembers())
+    }
+  
+  async getMembers() {
+    try {
+      return lastValueFrom(this.http.get('http://localhost:5285/api/members')) 
+    } catch (e) {
+      throw e
+    }
+
+  }
+
 }
